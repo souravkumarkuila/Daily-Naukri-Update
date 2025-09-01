6793c0cea785a7ba28b2c1eee6a566997df4d0d7
Sourav Kumar kuila <93852634+souravkumarkuila@users.noreply.github.com>
2025-09-01 12:43:48 +0530

    Create constant.py

---
+++ constant.py
@@ -0,0 +1,9 @@
+# Configuration and constants used in the script. Update values as needed.
+
+USERNAME = "kuila.sourav2001@gmail.com"
+PASSWORD = "Sourav@01101999"
+MOBILE = "8002233878"
+ORIGINAL_RESUME_PATH="C:/sourav-kuila-Devops-resume.pdf"
+MODIFIED_RESUME_PATH="C:/sourav-kuila-Devops-resume.pdf"
+NAUKRI_LOGIN_URL="https://www.naukri.com/nlogin/login"
+NAUKRI_PROFILE_URL="https://www.naukri.com/mnjuser/profile"
